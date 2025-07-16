2b015d6c047ad2db3c4f7bb07b7486558971d25f
KaterinaZhidkova <KaterinaZhidkova@users.noreply.github.com>
2025-07-16 23:38:06 +0000

    deploy: 71eaaf7ef79aa6789d2fdd0be3a17375188b1e39

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1412618777"
+    serviceWorkerVersion: "250973532"
   }
 });
